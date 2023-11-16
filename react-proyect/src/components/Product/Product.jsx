46cc1a07f683a64c6b9d1da23907ca9284c59b9d
matu.dev <matu.a.gimenez2002@gmail.com>
2023-11-16 16:28:16 -0300

    clase 6

--- react-proyect/src/components/Product/Product.jsx
+++ react-proyect/src/components/Product/Product.jsx
@@ -2,34 +2,58 @@ import React, { useState } from 'react'
 
 const Product = (propiedades) => {
     const [estaComprado, setEstaComprado] = useState(false)
-
-    
-    const handleAddToCart = () =>{
+    const [counter, setCounter] = useState(0)
+    const [showBox, setShowBox] = useState(false)
+    const handleAddToCart = () => {
         setEstaComprado(true)
     }
 
-     /* 
-     Creen un estado que se llame showBox y con un valor inicial el false
-     Luego tambien creen un div que mida 200*200 y sea de color violeta
-     Luego creen un button que diga mostrar
+    const handleChangeVisibility = () => {
+        setShowBox(!showBox)
+    }
 
-     Si showBox es verdadero el div se mostrara
+    const [counterVisibility, setCounterVisibility] = useState(false)
+    /* 
+    Creen un estado que se llame showBox y con un valor inicial el false
+    Luego tambien creen un div que mida 200*200 y sea de color violeta
+    Luego creen un button que diga mostrar
 
-     Cuando le den click al botton mostrar debera mostrarnos el div
-     */
+    Si showBox es verdadero el div se mostrara
 
-    console.log('me cargo')
+    Cuando le den click al botton mostrar debera mostrarnos el div
+    */
+    const addToCart = () =>{
+        setCounterVisibility(true)
+        setCounter(1)
+    }
+    const cancelCart = () => {
+        setCounterVisibility(false)
+        setCounter(0)
+    }
+    
+    
     return (
         <div>
             {propiedades.favorito && <button>Quitar de favoritos</button>}
             <h2>{propiedades.nombre}</h2>
             <h3>Precio: ${propiedades.precio}</h3>
+
             {
-                estaComprado 
-                ?<button>Quitar del carro</button>
-                :<button onClick={handleAddToCart}>Agregar al carro</button>
+                counterVisibility 
+                ? (
+                    <div>
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                    <button onClick={cancelCart}>Cancelar</button>
+                    <button>Confirmar</button>
+                </div>
+                )
+                :<button onClick={addToCart}>Agregar al carrito</button>
             }
             
+            
+            <button onClick={handleChangeVisibility}>{showBox ? 'Ocultar' : 'Mostrar'}</button>
         </div>
     )
 }
@@ -64,4 +88,41 @@ div
 estaOculto vamos a usarlo como condicionar si se muestra o no el tipo peso y fecha
 
 
+*/
+
+
+/* 
+Vamos a crear un boton en Product que diga
+
+agregar al carrito
+
+Y un contador que tenga la sig estructura
+
+<div>
+    <button>-</button>
+    <span>1</span>
+    <button>+</button>
+    <button>Cancelar</button>
+    <button>Confirmar</button>
+</div>
+
+Crear el estado llamado counterVisibility
+Si counterVisibility es true debera mostrarse el contador
+Sino debera mostrarse el button para agregar al carrito
+
+
+Al darle click al boton agregar al carrito counterVisibility sera verdadero
+
+
+
+Crear un estado llamado counter que empieze en 0, 
+si le doy click al button de + debera incrementar en 1 el contador
+si le doy click al button de - debera decrementar en 1 el contador
+
+NO PUEDE SER MENOS QUE 1 el contador
+
+Si le doy click al button de cancelar debera hacer que el contador valga 0 y se oculte otra vez el contador
+
+
+Al darle click a agregar al carrito el counter sera 1 tambien
 */
